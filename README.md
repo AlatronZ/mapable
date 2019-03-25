1cafb944dc173914b8465871dd90db9cb0a70f26
Alastair Chin <Alastair@iPhone-de-Bruna.localdomain>
2019-03-26 11:17:33 +1300

    Basic mappable displayed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mapable
 Polygon drawing on maps
+
+Latitude and Longitude coordinates can be converted from locations via https://www.latlong.net/
--- src/app.js
+++ src/app.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import L from 'leaflet-draw';
+import L from 'leaflet';
+const leafletDraw = require('leaflet-draw');
 
-//const leafletDraw = require('leaflet-draw');
+const Mapable = () => {
+    return (
+        <div>
+            <p>Hello I'm Mapable!</p>
+            <div
+                id="mapid"
+                style={{ height: "480px", width: "480px" }}>
+                
+            </div>
+        </div>
+    )
+}
 
-// var map = L.map('map', {drawControl: true}).setView([51.505, -0.09], 13);
-//      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-//          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//      }).addTo(map);
+ReactDOM.render(<Mapable />, document.getElementById('app'));
 
-// class Mapable extends React.Component {
-    
-// }
+//Map can only be created after the connected map object
+var myMap = L.map('mapid', { drawControl: true }).setView([-41.294018, 174.777596], 13);
 
+//Create a tile layer of which the map will be used
+/*
+* {s} subdomain, {z} zoom, {x}{y} coordinates, {r}(optional) load retina tiles
+* While using any part of OSM it must be an attribution/reference per copyright
+*/
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
+var polygon = L.polygon([
+    [-41.0, 1745.0],
+    [-20, 1780],
+    [-36, 1800]
+], {
+    color: 'red'
+}).addTo(myMap);
 
-ReactDOM.render(<p>Hello I'm Mapable!</p>, document.getElementById('app'));
+//circle works
+// var circle = L.circle([-41.294018, 174.777596], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(myMap);
+
+ReactDOM.render(<Mapable />, document.getElementById('app'));
