4bde40bf725909d5d8898bf3ba34b98346a238c4
Alastair Chin <Alastair@iPhone-de-Bruna.localdomain>
2019-03-26 12:07:44 +1300

    Fixed polygon coords, added marker and click

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import L from 'leaflet';
 const leafletDraw = require('leaflet-draw');
 
+//Contains style overrides
 const Mapable = () => {
     return (
         <div>
-            <p>Hello I'm Mapable!</p>
+        <h1>Welcome to Mappable!</h1>
+            <p>Look at this map, isn't it neat <br />
+            Unfortunately this code is incomplete</p>
             <div
                 id="mapid"
                 style={{ height: "480px", width: "480px" }}>
-                
             </div>
         </div>
     )
@@ -18,8 +20,9 @@ const Mapable = () => {
 
 ReactDOM.render(<Mapable />, document.getElementById('app'));
 
+const latLanHome = [-41.294018, 174.777596];
 //Map can only be created after the connected map object
-var myMap = L.map('mapid', { drawControl: true }).setView([-41.294018, 174.777596], 13);
+const myMap = L.map('mapid').setView(latLanHome, 13);
 
 //Create a tile layer of which the map will be used
 /*
@@ -30,20 +33,29 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-var polygon = L.polygon([
-    [-41.0, 1745.0],
-    [-20, 1780],
-    [-36, 1800]
+/* Using an image test
+* It works but needs a large enough image or it literally tile layers
+Could then add conditions of max/min zoom
+// */
+// L.tileLayer('/images/MapImage.png').addTo(myMap);
+
+const polygon = L.polygon([
+    [-41.3, 174.8],
+    [-41.32, 174.75],
+    [-41.28, 174.8]
 ], {
     color: 'red'
 }).addTo(myMap);
 
-//circle works
-// var circle = L.circle([-41.294018, 174.777596], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(myMap);
+//potential future use
+const marker = L.marker([-41.294018, 174.777596]);
+marker.addTo(myMap);
+marker.bindPopup("Abletech");
 
-ReactDOM.render(<Mapable />, document.getElementById('app'));
+var popup = L.popup().setLatLng([-41.3, 174.8]).setContent("Coordinate").openOn(myMap);
+
+//Click event handler
+const onMapClick = (e) => {
+    console.log(e);
+}
+myMap.on('click', onMapClick);
