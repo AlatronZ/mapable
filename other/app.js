e0df8aec0a4cc14d3f8b4b114c10ba33713e290f
AlatronZ <alastair.chin@abletech.co.nz>
2019-03-27 12:52:02 +1300

    Tidy up, toolbar added

--- other/app.js
+++ other/app.js
@@ -1,81 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import L from 'leaflet';
+import 'leaflet-draw';
+
+import 'normalize.css/normalize.css';
+import '../src/styles/styles.scss';
 
 /**
  * This version of mappable uses leaflet js instead of react leaflet 
  */
 
-//Contains style overrides
+//Contains style overrides to view map window
 const Mapable = () => {
-    return (
-        <div>
-            <h1>Welcome to Mapable!</h1>
-            <p>Look at this map, isn't it neat <br />
-                Unfortunately this code is incomplete</p>
-            <div
-                id="mapid"
-                style={{ height: "480px", width: "480px" }}>
-            </div>
+    return (<div>
+        <h1 > Welcome to Mapable! </h1>
+        <p> Look at this map, isn 't it neat <br />
+            Unfortunately this code is incomplete </p>
+        <div id="myMap" >
         </div>
+    </div>
     )
 }
 
-ReactDOM.render(<Mapable />, document.getElementById('app'));
+ReactDOM.render(< Mapable />, document.getElementById('app'));
 
 const latLanHome = [-41.294018, 174.777596];
 //Map can only be created after the connected map object
-const myMap = L.map('mapid').setView(latLanHome, 13);
+const myMap = L.map('myMap').setView(latLanHome, 13);
 
-//Create a tile layer of which the map will be used
-/*
-* {s} subdomain, {z} zoom, {x}{y} coordinates, {r}(optional) load retina tiles
-* While using any part of OSM it must be an attribution/reference per copyright
-*/
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-
-/*
- * Later test for addressfinder tile layers
+/*Create a tile layer of which the map will be used
+*
+ * {s} subdomain, {z} zoom, {x}{y} coordinates, {r}(optional) load retina tiles
+ * While using any part of OSM it must be an attribution/reference per copyright
  */
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-/* Using an image test
-* It works but needs a large enough image or it literally tile layers
-Could then add conditions of max/min zoom
-// */
-// L.tileLayer('/images/MapImage.png').addTo(myMap);
+var drawnItems = new L.FeatureGroup();
+myMap.addLayer(drawnItems);
 
-const polygon1 = L.polygon([
-    [-41.3, 174.8],
-    [-41.32, 174.75],
-    [-41.28, 174.8]
-], {
-        color: 'red'
-    }).addTo(myMap);
+var drawControl = new L.Control.Draw({
+    position: 'topleft',
+    draw: {
+        marker: true,
+        polygon: {
+            title: 'Draw a polygon!',
+            allowIntersection: false,
+            drawError: {
+                color: 'red',
+                message: 'Don\'t cross the streams!',
+                timeout: 1500
+            },
+            shapeOptions: {
+                color: 'green'
+            }
+        },
 
-let clickList = [];
+        //disable
+        circle: false,
+        circlemarker: false,
+        polyline: false,
+        rectangle: false
+    },
+    edit: {
+        featureGroup: drawnItems,
+        remove: false
+    }
+});
+myMap.addControl(drawControl);
 
-//potential future use
+let editableCanvas = new L.FeatureGroup();
+myMap.addLayer(editableCanvas);
+
 const marker = L.marker([-41.294018, 174.777596]);
-marker.addTo(myMap);
+marker.addTo(editableCanvas);
 marker.bindPopup("Abletech");
 
-var popup = L.popup().setLatLng([-41.3, 174.8]).setContent("Coordinate").openOn(myMap);
+myMap.on('draw:created', (e) => {
+    if(e.layerType==='polygon'){
+        e.layer.addTo(editableCanvas);
+    } else if(e.layerType==='marker'){
+        e.layer.addTo(editableCanvas);
+    }
+})
 
 //Click event handler
 const onMapClick = (e) => {
     //need a toolbar or set point limit
-    console.log(`Point ${clickList.length}: ${e.latlng}`);
-    clickList = [...clickList, [e.latlng.lat, e.latlng.lng]];
-    //implemented with point limit of 3
-    if (clickList.length >= 3) {
-        console.log('Polygon complete');
-        const polygon2 = L.polygon(clickList, { color: 'purple' }).addTo(myMap);
-        clickList = [];
-    }
+    //drawPolygon(e)
 }
 myMap.on('click', onMapClick);
---
+++ other/playStuff.js
@@ -0,0 +1,32 @@
+/**
+ * JS file of deleted code from original file but kept for reference
+ */
+
+//----------Playing with polygons on Map----------
+// const polygon1 = L.polygon([
+//     [-41.3, 174.8],
+//     [-41.32, 174.75],
+//     [-41.28, 174.8]
+// ], {
+//         color: 'red'
+//     }).addTo(myMap);
+//var popup = L.popup().setLatLng([-41.3, 174.8]).setContent("Coordinate").openOn(myMap);
+
+// let clickList = [];
+// const drawPolygon = (e) => {
+//     console.log(`Point ${clickList.length}: ${e.latlng}`);
+//     clickList = [...clickList, [e.latlng.lat, e.latlng.lng]];
+//     //implemented with point limit of 3
+//     if (clickList.length >= 3) {
+//         console.log('Polygon complete');
+//         const polygon2 = L.polygon(clickList, { color: 'purple' }).addTo(myMap);
+//         clickList = [];
+//     }
+// }
+
+//----------Tile Layer Image Test----------
+/* Using an image test
+* It works but needs a large enough image or it literally tile layers
+Could then add conditions of max/min zoom
+// */
+// L.tileLayer('/images/MapImage.png').addTo(myMap);
